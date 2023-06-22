4bae08339b67db8f61535fe7cef1feb339f6c73e
arishuy <huybui19898@gmail.com>
2023-06-22 15:26:16 +0700

    add page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,29 @@
+"use client"
 import React from 'react'
-
-const page = () => {
+import { Grid } from "@mui/material";
+import TopMessage from '@/components/dashboard/TopMessage';
+import Reminder from '@/components/dashboard/Reminder';
+const Page = () => {
   return (
-    <div>page</div>
+    
+    <div>
+        <title>Dashboard</title>
+        <Grid
+			container
+			spacing={3}
+			sx={{
+				width: "auto",
+				marginLeft: "auto",
+			}}>
+                <Grid item xs={12} md={4} lg={4}>
+                    <TopMessage />
+                </Grid>
+                <Grid item xs={12} md={8} lg={8}>
+                    <Reminder />
+                </Grid>
+                </Grid>
+    </div>
   )
 }
 
-export default page
+export default Page
