5487abbb7b916a3a8a4019839f4196e363c6ef15
sumori <hoangthiphuonguyen1602@gmail.com>
2023-06-22 11:50:04 +0700

    add model

---
+++ src/models/message.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const messageSchema = new Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: { type: String, trim: true },
+    parentMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    hearts: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    createAt: { type: Date, default: Date.now },    
+    isReminder: { type: Boolean, default: false },
+    reminder: { type: mongoose.Schema.Types.ObjectId, ref: "Reminder" },
+  },
+
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
