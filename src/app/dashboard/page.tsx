78c1a6d1379e318d567f33810453443d0363257f
arishuy <huybui19898@gmail.com>
2023-06-23 11:20:53 +0700

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,28 @@
 "use client"
-"use client"
 import React from 'react'
 import { Grid } from "@mui/material";
 import TopMessage from '@/components/dashboard/TopMessage';
 import Reminder from '@/components/dashboard/Reminder';
 const Page = () => {
   return (
-    <div>page</div>
+    
+    <div>
+        <title>Dashboard</title>
+        <Grid
+			container
+			spacing={3}
+			sx={{
+				width: "auto",
+				marginLeft: "auto",
+			}}>
+                <Grid item xs={12} md={4} lg={4}>
+                    <TopMessage />
+                </Grid>
+                <Grid item xs={12} md={8} lg={8}>
+                    <Reminder />
+                </Grid>
+                </Grid>
+    </div>
   )
 }
 
