d62ff10ed64540b14596cf276ce3f65aac05fde6
sumori <hoangthiphuonguyen1602@gmail.com>
2023-06-27 00:21:50 +0700

    add upload user avatar

---
+++ src/components/profile/UpdateProfile.tsx
@@ -0,0 +1,257 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import { useRef, useEffect, useState } from "react";
+import AvatarUploadTrigger from "./avatar/AvatarUploadTrigger";
+import RootModal from "../modals/RootModal"
+import AvatarEditor from "./avatar/AvatarEditor";
+import { getCroppedImg } from "./avatar/AvatarEditor";
+import useResponsive from "@/hooks/useResponsive";
+import useTranslation from "next-translate/useTranslation";
+import axios from "axios";
+
+const UpdateProfile = ({
+  userInfo,
+  handleOk,
+  handleGetImage,
+  profileImage,
+}: {
+  userInfo: any;
+  handleOk: any;
+  handleGetImage: any;
+  profileImage: any;
+}) => {
+  const { t } = useTranslation();
+  const updatedUserInfo = useRef<any>();
+  const [file, setFile] = useState("");
+  const [open, setOpen] = useState(false);
+  const imgRef = useRef<any>(null);
+  const [completedCrop, setCompletedCrop] = useState<any>(null);
+  const isMobile = useResponsive("down", "sm");
+  const handleUpdateUserInfo = async () => {
+    const data = {
+      userName: updatedUserInfo.current.userName,
+      email: updatedUserInfo.current.email,
+      name: updatedUserInfo.current.name,
+      surname: updatedUserInfo.current.surname,
+      phoneNumber: updatedUserInfo.current.phoneNumber,
+    };
+    const response = await fetch(`/api/identity/profile/my-profile`, {
+      method: "PUT",
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    if (result.success) {
+      handleOk();
+    }
+  };
+  const onFinishUpload = () => {
+    setOpen(false);
+  };
+  const uploadImage = async () => {
+    if (imgRef.current && completedCrop) {
+      const img = await getCroppedImg(imgRef.current, completedCrop, "avatar");
+      console.log(img);
+       const formData = new FormData();
+       formData.append("file", img);
+      formData.append("upload_preset", "a7i0et5j");
+      try {
+        const response = await axios.post(
+          `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+          formData
+        );
+          // handleGetImage();
+        console.log(response);
+        await fetch(`/api/user/image`, {
+          method: "POST",
+          body: JSON.stringify({
+            avatar: response.data.secure_url,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+          onFinishUpload();
+
+      } catch (error) {
+        console.error(error);
+      }
+      // const base64Img = img.split(",").slice(1).join(",");
+      // const response = await fetch("/api/identity/profile/my-avatar", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify(base64Img),
+      // });
+      // const result = await response.json();
+      // if (result.success) {
+      //   handleGetImage();
+      //   onFinishUpload();
+      // }
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      updatedUserInfo.current = userInfo;
+    }
+  }, [userInfo]);
+
+  return (
+    userInfo && (
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          width: "100%",
+          margin: "auto",
+          padding: isMobile ? "0px" : "auto",
+        }}
+      >
+        <RootModal
+          title="Crop Image"
+          variant="Create"
+          open={open}
+          handleClose={onFinishUpload}
+          handleOk={uploadImage}
+          closeOnly={false}
+        >
+          <AvatarEditor
+            sourceImg={file}
+            onUploadImage={(uploadImage: any) => {
+              imgRef.current = uploadImage;
+            }}
+            setCompletedCrop={(completedCrop) => {
+              setCompletedCrop(completedCrop);
+            }}
+          />
+        </RootModal>
+        <Grid item xs={12} md={4} sx={{ paddingLeft: "0px !important" }}>
+          <Card
+            sx={{
+              borderRadius: "20px",
+              height: isMobile ? "350px" : "429px",
+              width: "100%",
+            }}
+          >
+            <AvatarUploadTrigger
+              profileImage={profileImage}
+              onNewSelectedFile={(file) => {
+                setFile(file);
+                setOpen(true);
+              }}
+            />
+          </Card>
+        </Grid>
+        <Grid
+          item
+          spacing={3}
+          xs={12}
+          md={8}
+          sx={{
+            ...(isMobile && { paddingLeft: "0px !important" }),
+          }}
+        >
+          <Card sx={{ padding: "20px", borderRadius: "20px" }}>
+            <Grid
+              container
+              spacing={2}
+              sx={{
+                "& .css-n26ql7-MuiInputBase-input-MuiOutlinedInput-input": {
+                  padding: isMobile ? "16px 16px" : "auto",
+                  fontSize: isMobile ? "15px" : "auto",
+                },
+                "& label": {
+                  fontSize: isMobile ? "15.5px" : "auto",
+                },
+              }}
+            >
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Username"
+                  fullWidth
+                  defaultValue={userInfo.username || ""}
+                  onChange={(e: any) => {
+                    updatedUserInfo.current.userName = e.target.value;
+                  }}
+                  sx={{ marginBottom: "13px", marginTop: "13px" }}
+                />
+              </Grid>
+              <Grid item xs={isMobile ? 12 : 6}>
+                <TextField
+                  id="outlined-required"
+                  label="Name"
+                  fullWidth
+                  defaultValue={userInfo?.name || ""}
+                  onChange={(e: any) => {
+                    updatedUserInfo.current.name = e.target.value;
+                  }}
+                  sx={{ marginBottom: "13px" }}
+                />
+              </Grid>
+              <Grid item xs={isMobile ? 12 : 6}>
+                <TextField
+                  id="outlined-required"
+                  label="Surname"
+                  fullWidth
+                  defaultValue={userInfo?.surname || ""}
+                  onChange={(e: any) => {
+                    updatedUserInfo.current.surname = e.target.value;
+                  }}
+                  sx={{ marginBottom: "13px" }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Email"
+                  fullWidth
+                  defaultValue={userInfo?.email || ""}
+                  onChange={(e: any) => {
+                    updatedUserInfo.current.email = e.target.value;
+                  }}
+                  sx={{ marginBottom: "13px" }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-required"
+                  label="Quote"
+                  fullWidth
+                  defaultValue={userInfo?.quote || ""}
+                  onChange={(e: any) => {
+                    updatedUserInfo.current.phoneNumber = e.target.value;
+                  }}
+                  sx={{ marginBottom: "13px" }}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sx={{ display: "flex", justifyContent: "flex-end" }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{
+                    borderRadius: 1,
+                  }}
+                  onClick={handleUpdateUserInfo}
+                >
+                  {t("common:SaveChanges")}
+                </Button>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+    )
+  );
+};
+
+export default UpdateProfile;
